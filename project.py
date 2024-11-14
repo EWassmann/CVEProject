84b7470d98cd36f048a99e309777c21f796fe116
EWassmann <evanjwassmann@gmail.com>
2024-11-14 15:03:43 -0500

    made code run with example pointcloud

--- project.py
+++ project.py
@@ -2,6 +2,8 @@ import cv2
 import numpy as np
 import pyrealsense2 as rs
 import open3d as o3d
+import matplotlib.pyplot as plt
+#import open3d_tutorial as o3dtut
 '''
 First part of the project
 1. Start video stream to realsense camera
@@ -26,80 +28,80 @@ Likely want to do some preprocessing as well
 # https://github.com/AoLyu/Some-implementions-with-RGBD-camera-RealSense-D435/blob/62364d118cef39bf01f6b7bdc7a4ef3edaacf57a/Basic/captureRGBDpt.py#L102
 # https://medium.com/@christhaliyath/lidar-and-pointcloud-simple-tutorial-for-op-3c5d7cd35ad4
 
-try:
-    # Create a pipline to handle the realsense camera
-    pipeline = rs.pipeline()
+# try:
+#     # Create a pipline to handle the realsense camera
+#     pipeline = rs.pipeline()
     
-    # Configure video streams
-    config = rs.config()
+#     # Configure video streams
+#     config = rs.config()
     
-    # Capture both depth and color data 
-    config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
-    config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
+#     # Capture both depth and color data 
+#     config.enable_stream(rs.stream.depth, 640, 480, rs.format.z16, 30)
+#     config.enable_stream(rs.stream.color, 640, 480, rs.format.bgr8, 30)
     
-    # Start streaming and obtain camera intrinsics
-    profile = pipeline.start(config)
-    depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
-    intrinsics = profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
-    print((intrinsics.width, intrinsics.height, intrinsics.fx, intrinsics.fy, intrinsics.ppx, intrinsics.ppy))
-    pinhole_intrinsics = o3d.camera.PinholeCameraIntrinsic(intrinsics.width, intrinsics.height, intrinsics.fx, intrinsics.fy, intrinsics.ppx, intrinsics.ppy)
+#     # Start streaming and obtain camera intrinsics
+#     profile = pipeline.start(config)
+#     depth_scale = profile.get_device().first_depth_sensor().get_depth_scale()
+#     intrinsics = profile.get_stream(rs.stream.color).as_video_stream_profile().get_intrinsics()
+#     print((intrinsics.width, intrinsics.height, intrinsics.fx, intrinsics.fy, intrinsics.ppx, intrinsics.ppy))
+#     pinhole_intrinsics = o3d.camera.PinholeCameraIntrinsic(intrinsics.width, intrinsics.height, intrinsics.fx, intrinsics.fy, intrinsics.ppx, intrinsics.ppy)
     
-    # Capture frames
-    for _ in range(30): # need to adjust the frame counts based on our use case
-        frames = pipeline.wait_for_frames()
-        depth_frame = frames.get_depth_frame()
-        color_frame = frames.get_color_frame()
+#     # Capture frames
+#     for _ in range(30): # need to adjust the frame counts based on our use case
+#         frames = pipeline.wait_for_frames()
+#         depth_frame = frames.get_depth_frame()
+#         color_frame = frames.get_color_frame()
         
-        if (depth_frame == None) or (color_frame == None):
-            print("Depth frames or color frames are not captured.\n")
-            continue
+#         if (depth_frame == None) or (color_frame == None):
+#             print("Depth frames or color frames are not captured.\n")
+#             continue
         
-        # Obtain Open3D images for depth, color, and RGBD
-        depth_image = o3d.geometry.Image(np.asanyarray(depth_frame.get_data()))
-        color_image = o3d.geometry.Image(np.asanyarray(color_frame.get_data()))
-        rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_image, depth_image, convert_rgb_to_intensity=False)
+#         # Obtain Open3D images for depth, color, and RGBD
+#         depth_image = o3d.geometry.Image(np.asanyarray(depth_frame.get_data()))
+#         color_image = o3d.geometry.Image(np.asanyarray(color_frame.get_data()))
+#         rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_image, depth_image, convert_rgb_to_intensity=False)
         
-        # Generate point clouds
-        point_cloud = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, pinhole_intrinsics)
+#         # Generate point clouds
+#         point_cloud = o3d.geometry.PointCloud.create_from_rgbd_image(rgbd_image, pinhole_intrinsics)
         
-        # To standardize coordinate system
-        matrix = [
-            [1, 0, 0, 0],  # X - Remains the same
-            [0, -1, 0, 0], # Y - Inverted
-            [0, 0, -1, 0], # Z - Inverted
-            [0, 0, 0, 1]   # No translation
-        ]
-        point_cloud.transform(matrix)
+#         # To standardize coordinate system
+#         matrix = [
+#             [1, 0, 0, 0],  # X - Remains the same
+#             [0, -1, 0, 0], # Y - Inverted
+#             [0, 0, -1, 0], # Z - Inverted
+#             [0, 0, 0, 1]   # No translation
+#         ]
+#         point_cloud.transform(matrix)
         
-        # Need to be modified based on depth (in meters)
-        threshold = 0.1
-        point_cloud_points = np.asarray(point_cloud.points)
-        point_cloud_rgb = np.asarray(point_cloud.colors)
+#         # Need to be modified based on depth (in meters)
+#         threshold = 0.1
+#         point_cloud_points = np.asarray(point_cloud.points)
+#         point_cloud_rgb = np.asarray(point_cloud.colors)
         
-        # Create a mask for point cloud (z-axis)
-        mask = point_cloud_points[:, 2] < threshold
+#         # Create a mask for point cloud (z-axis)
+#         mask = point_cloud_points[:, 2] < threshold
         
-        # Filter out points and RGB
-        filtered_points = point_cloud_points[mask]
-        filtered_rgb = point_cloud_rgb[mask]
+#         # Filter out points and RGB
+#         filtered_points = point_cloud_points[mask]
+#         filtered_rgb = point_cloud_rgb[mask]
         
-        # Visualize masked point cloud 
-        point_cloud_masked = o3d.geometry.PointCloud()
-        point_cloud_masked.points = o3d.utility.Vector3dVector(filtered_points)
-        point_cloud_masked.colors = o3d.utility.Vector3dVector(filtered_rgb)
+#         # Visualize masked point cloud 
+#         point_cloud_masked = o3d.geometry.PointCloud()
+#         point_cloud_masked.points = o3d.utility.Vector3dVector(filtered_points)
+#         point_cloud_masked.colors = o3d.utility.Vector3dVector(filtered_rgb)
         
-        o3d.visualization.draw_geometries([point_cloud_masked])
+#         o3d.visualization.draw_geometries([point_cloud_masked])
         
-        # Save the masked point cloud
-        o3d.io.write_point_cloud("masked_point_cloud.ply", point_cloud_masked)
+#         # Save the masked point cloud
+#         o3d.io.write_point_cloud("masked_point_cloud.ply", point_cloud_masked)
         
-# For debugging purposes
-except Exception as e:
-    print(e)
-    pass
+# # For debugging purposes
+# except Exception as e:
+#     print(e)
+#     pass
 
 
-
+print("1")
 
 # second part of project, find the transforms between the depth frames (maybe use icp) and if we are unable to mask the point clouds before we find the transforms, apply the masks to the depth images now and create smaller point clouds to apply the same transforms to
 #section output- a single point cloud representing the object.
@@ -114,15 +116,39 @@ def prepare_dataset(source, target, voxel_size):
     target_down, target_fpfh = preprocess_point_cloud(target, voxel_size)
     return source, target, source_down, target_down, source_fpfh, target_fpfh
 
+
+mesh_rabbit_info = o3d.data.BunnyMesh() #o3d.get_bunny_mesh()
+#print(mesh_rabbit_info.path)
+mesh_rabbit=o3d.io.read_triangle_mesh(mesh_rabbit_info.path)
+
+pcd_rabbit = mesh_rabbit.sample_points_poisson_disk(750)
+pcd_rabbit_transformed = mesh_rabbit.sample_points_poisson_disk(750)
+#o3d.visualization.draw_geometries([pcd_rabbit_transformed])
+print("1.1")
+t_M = np.asarray([[1,0,0,0],
+                [0,np.cos(20*np.pi/180),np.sin(20*np.pi/180),0],
+                [0,-np.sin(20*np.pi/180),np.cos(20*np.pi/180),0],
+                [0,0,0,1]])
+T = t_M.astype(np.float64)
+
+T = T.astype(np.float64)
+# T[:3, :3] = mesh_rabbit.get_rotation_matrix_from_xyz((0, np.pi / 3, np.pi / 2))
+# T[0, 3] = 1
+# T[1, 3] = 1.3
+print(T)
+print("1.2")
+pcd_rabbit_transformed.transform(T)
+
+print("1.25")
 # Downsamples pointcloud source and target 
 def preprocess_point_cloud(pcd, voxel_size):
-    pcd_down = pcd.voxel_down_sample(source, target, voxel_size)
+    pcd_down = pcd.voxel_down_sample(voxel_size)
     radius_normal = voxel_size * 2
     pcd_down.estimate_normals(o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
     radius_feature = voxel_size * 5
     pcd_fpfh = o3d.pipelines.registration.compute_fpfh_feature(pcd_down,o3d.geometry.KDTreeSearchParamHybrid(radius=radius_feature, max_nn=100))
     return pcd_down, pcd_fpfh
-
+print("1.5")
 # Runs RANSAC on downsampled pointclouds
 def execute_global_registration(source_down, target_down, source_fpfh, target_fpfh, voxel_size):
     distance_threshold = voxel_size * 1.5
@@ -134,25 +160,26 @@ def execute_global_registration(source_down, target_down, source_fpfh, target_fp
          o3d.pipelines.registration.RANSACConvergenceCriteria(100000, 0.999))
     return result
 
-
-for _ in depthFrames: # loop through frames in rotation order, or use an evaluation metric to compare two frames and make sure they have enough overlap
+print("2")
+#for _ in depthFrames: # loop through frames in rotation order, or use an evaluation metric to compare two frames and make sure they have enough overlap
     
-    voxel_size = 0.01  # means 1cm for this dataset
-    threshold = 0.02
-    source = o3d.geometry.PointCloud() # Loop through each frame
-    target = o3d.geometry.PointCloud() # set to base depth frame
-    
-    # evaluation = o3d.pipelines.registration.evaluate_registration(source, target, threshold, trans_init)
-    # print(evaluation)
-    
-    # Take data set and downsample point cloud to prepare for RANSAC
-    source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(source, target, voxel_size)
-    # Obtain rough tansformation matrix from RANSAC
-    result_ransac = execute_global_registration(source_down, target_down,source_fpfh, target_fpfh, voxel_size)
-    # Obtain refined transformation matrix from ICP, using RANSAC matrix as initial guess
-    reg_p2p = o3d.pipelines.registration.registration_icp(source, target, threshold, result_ransac.transformation,o3d.pipelines.registration.TransformationEstimationPointToPoint())
+voxel_size = 0.01  # means 1cm for this dataset
+threshold = 0.02
+#source = o3d.geometry.PointCloud() # Loop through each frame
+#target = o3d.geometry.PointCloud() # set to base depth frame
+source = pcd_rabbit
+target = pcd_rabbit_transformed
+# evaluation = o3d.pipelines.registration.evaluate_registration(source, target, threshold, trans_init)
+# print(evaluation)
 
+# Take data set and downsample point cloud to prepare for RANSAC
+source, target, source_down, target_down, source_fpfh, target_fpfh = prepare_dataset(source, target, voxel_size)
+# Obtain rough tansformation matrix from RANSAC
+result_ransac = execute_global_registration(source_down, target_down,source_fpfh, target_fpfh, voxel_size)
+# Obtain refined transformation matrix from ICP, using RANSAC matrix as initial guess
+reg_p2p = o3d.pipelines.registration.registration_icp(source, target, threshold, result_ransac.transformation,o3d.pipelines.registration.TransformationEstimationPointToPoint())
 
+print("3")
 
 #third part of project, do post processing on the point cloud as needed, turn it into a mesh probably, compare it to a mesh we read in? alighn it with that read in mesh?
 #section output- two meshes probably? transforms?
@@ -162,21 +189,21 @@ for _ in depthFrames: # loop through frames in rotation order, or use an evaluat
 
 #assuming from previous step we have: transfomred merged point clouds
 
-merged_point_clouds = o3d.geometry.PointCloud()
-##Alpha shaped
-alpha = 0.03 # tradeoff paramater- mess with changing this around
-print(f"alpha={alpha:.3f}")
-alpha_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(merged_point_clouds, alpha)
-alpha_mesh.compute_vertex_normals()
-o3d.visualization.draw_geometries([alpha_mesh], mesh_show_back_face=True)
-##Ball pivoting 
-## Next 2 techneques assume that point clouds have normals 
+merged_point_clouds = pcd_rabbit_transformed.transform(reg_p2p.transformation)
+# ##Alpha shaped
+# alpha = 0.03 # tradeoff paramater- mess with changing this around
+# print(f"alpha={alpha:.3f}")
+# alpha_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_alpha_shape(merged_point_clouds, alpha)
+# alpha_mesh.compute_vertex_normals()
+# o3d.visualization.draw_geometries([alpha_mesh], mesh_show_back_face=True)
+# ##Ball pivoting 
+# ## Next 2 techneques assume that point clouds have normals 
 merged_point_clouds.estimate_normals() #there are some diffrent paramaters in this we can change if we are not happy with the output
 merged_point_clouds.orient_normals_consistent_tangent_plane(100) #that 100 is the number of points to use in the graph to propogate normal orientation- may want to change
 
-radii = [0.005, 0.01, 0.02, 0.04] #these radii are " a parameter that corresponds to the radii of the individual balls that are pivoted on the point cloud." may want to change
-ball_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(merged_point_clouds, o3d.utility.DoubleVector(radii))
-o3d.visualization.draw_geometries([merged_point_clouds, ball_mesh])
+# radii = [0.005, 0.01, 0.02, 0.04] #these radii are " a parameter that corresponds to the radii of the individual balls that are pivoted on the point cloud." may want to change
+# ball_mesh = o3d.geometry.TriangleMesh.create_from_point_cloud_ball_pivoting(merged_point_clouds, o3d.utility.DoubleVector(radii))
+# o3d.visualization.draw_geometries([merged_point_clouds, ball_mesh])
 
 ## Poisson surface reconstruction, APPARENTLY SUPPOSED TO BE GOOD
 with o3d.utility.VerbosityContextManager(
@@ -185,19 +212,20 @@ with o3d.utility.VerbosityContextManager(
         merged_point_clouds, depth=9) #the larger the depth value is the more detail will be in the mesh
 ##density value can be visualized to show how many supporting points there are for each vertex
 print(Poisson_mesh)
-o3d.visualization.draw_geometries([Poisson_mesh],
-                                  zoom=0.664,
-                                  front=[-0.4761, -0.4698, -0.7434],
-                                  lookat=[1.8900, 3.2596, 0.9284],
-                                  up=[0.2304, -0.8825, 0.4101])
+o3d.visualization.draw_geometries([Poisson_mesh])
+# o3d.visualization.draw_geometries([Poisson_mesh],
+#                                   zoom=0.664,
+#                                   front=[-0.4761, -0.4698, -0.7434],
+#                                   lookat=[1.8900, 3.2596, 0.9284],
+#                                   up=[0.2304, -0.8825, 0.4101])
 
 #deoending on quality of mesh we can smooth it, there are several ways to do this. 
 
 
-
+print("4")
 #read in mesh of intrest,
-input_mesh = o3d.io.read_triangle_mesh("path to mesh, idk if it should be string") #
-
+# input_mesh = o3d.io.read_triangle_mesh("path to mesh, idk if it should be string") #
+input_mesh = mesh_rabbit
 #sounds like poisson disk sampling is the best way to get a point cloud from the mesh
 input_pcd = input_mesh.sample_points_poisson_disk(number_of_points=500, init_factor=5) #may want to mess with these inputs as well
 #now lets do point cloud regestration to find the transform, apply to meshes and compare 
@@ -218,32 +246,35 @@ dists = merged_point_clouds.compute_point_cloud_distance(input_pcd) #doistance b
 #global regestration on the two of them.
 
 
-
+print("5")
 
 #fourth part of the project, visualize the two meshes, diffrences between them, make good graphics to show off what we did before
 
 #%% Colored Difference 3D Point Cloud
 
 # Set a threshold for min difference
+dists = np.asarray(dists)
 threshold = 0.01
-difference_mask = dists > threshold
+# difference_mask = dists > threshold
+ind = np.where(dists > 0.01)[0]
 
 # Extract points and distances that differ significantly
-difference_points = merged_point_clouds[difference_mask]
-difference_colors = np.zeros((difference_points.shape[0], 3))  # initialize colors array
+difference_points = merged_point_clouds.select_by_index(ind)
+
+difference_colors = np.zeros((np.asarray(difference_points.points).shape[0], 3))  # initialize colors array
 
 # Map distances to colors for visualization (e.g., red for large differences)
 max_distance = dists.max()
-difference_colors[:, 0] = distances[difference_mask] / max_distance  # Red intensity based on difference
+difference_colors[:, 0] = dists[ind] / max_distance  # Red intensity based on difference
 
 # Create a new point cloud with highlighted differences
-diff_pcd = o3d.geometry.PointCloud()
-diff_pcd.points = o3d.utility.Vector3dVector(difference_points)
+diff_pcd = difference_points #o3d.geometry.PointCloud()
+#diff_pcd.points = o3d.utility.Vector3dVector(difference_points)
 diff_pcd.colors = o3d.utility.Vector3dVector(difference_colors)
 
 # Visualize the difference point cloud
 o3d.visualization.draw_geometries([diff_pcd])
-
+print("6")
 #%% Layer View
 
 # Convert point clouds to numpy arrays
